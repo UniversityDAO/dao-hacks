64d53a910677ab7e927a0d5cdd2e37e8c7e55965
Jagger-Harris <jagger.lee.harris@gmail.com>
2022-04-09 19:39:20 -0500

    Fixed event and added MIT license

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
 contract VotingContract {
@@ -39,6 +39,8 @@ contract VotingContract {
         _;
     }
 
+    event broadCastProposal(string _ipfsHashEvent, uint fundAmount, address _senderEvent);
+
     function submitProposal(string memory _ipfsHash, uint fundAmount) public {
         address sender;
         sender = msg.sender;
@@ -46,47 +48,46 @@ contract VotingContract {
     }
 
     function createProposal(string memory _proposalHash, uint fundAmount) external isMember {
-            Proposal storage proposal = proposals[_proposalHash];
+        Proposal storage proposal = proposals[_proposalHash];
 
-            proposal.open = true;
-            proposal.yesVotes = 0;
-            proposal.noVotes = 0;
-            proposal.amountOfFunds = fundAmount;
+        proposal.open = true;
+        proposal.yesVotes = 0;
+        proposal.noVotes = 0;
+        proposal.amountOfFunds = fundAmount;
 
-            proposalCount++;
+        proposalCount++;
     }
 
     function tallyVote(string memory _proposalHash, VoteType _vote) external isMember {
-            Proposal storage proposal = proposals[_proposalHash];
-            require(proposal.voters[msg.sender] == false, "Member has already voted");
-            require(proposal.open == true, "Proposal is closed for voting");
+        Proposal storage proposal = proposals[_proposalHash];
+        require(proposal.voters[msg.sender] == false, "Member has already voted");
+        require(proposal.open == true, "Proposal is closed for voting");
 
-            proposal.voters[msg.sender] = true;
+        proposal.voters[msg.sender] = true;
 
-            uint votingPower = members[msg.sender].userNFTs.length;
+        uint votingPower = members[msg.sender].userNFTs.length;
 
-            if (_vote == VoteType.YES)
-            {
-                proposal.yesVotes += votingPower;
-            }
-            else
-            {
-                proposal.noVotes += votingPower;
-            }
+        if (_vote == VoteType.YES)
+        {
+            proposal.yesVotes += votingPower;
+        }
+        else
+        {
+            proposal.noVotes += votingPower;
+        }
     }
 
     function releaseFunds (string memory _proposalHash) external isMember {
-            Proposal storage proposal = proposals[_proposalHash];
+        Proposal storage proposal = proposals[_proposalHash];
             
-            proposal.open = false;
-            if (proposal.yesVotes > proposal.noVotes)
-            {
-                //Do Something here 
-            }
-            else
-            {
-                //Do something here
-            }
-
+        proposal.open = false;
+        if (proposal.yesVotes > proposal.noVotes)
+        {
+            //Do Something here 
+        }
+        else
+        {
+            //Do something here
+        }
     }
 }
